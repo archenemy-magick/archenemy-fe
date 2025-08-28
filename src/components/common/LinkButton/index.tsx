20dce730fdb446869a4e174ad2d1efefbf64a698
HAH <hhoburg@gmail.com>
2025-08-28 15:49:13 -0600

    fixes LinkButton types

--- src/components/common/LinkButton/index.tsx
+++ src/components/common/LinkButton/index.tsx
@@ -2,7 +2,14 @@ import { Button } from "@mantine/core";
 import Link from "next/link";
 
 // TODO: give this actual props. Limit the props?
-const LinkButton = (props) => {
+import { ButtonProps } from "@mantine/core";
+
+interface LinkButtonProps extends ButtonProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const LinkButton = (props: LinkButtonProps) => {
   return (
     <Button component={Link} {...props}>
       {props.children}
